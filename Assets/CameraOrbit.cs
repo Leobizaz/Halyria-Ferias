2f1037fbf046ce2f0d028ae318a58d21a3aa7a94
Leobizaz <leobizaz12@gmail.com>
2019-07-14 19:17:05 -0300

    Orbita funcional

---
+++ Assets/CameraOrbit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    public Transform cameraOrbit;
+    public Transform target;
+
+    void Start()
+    {
+        cameraOrbit.position = target.position;
+        cameraOrbit.rotation = Quaternion.Euler(0, 90, 90);
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0);
+
+        transform.LookAt(target.position);
+    }
+}
