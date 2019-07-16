b1cc380c049c80cc7ffec0565e904350bc57e793
GuiSR388 <guicrilopez@users.noreply.github.com>
2019-07-16 15:30:31 -0300

    Alguns Assets adicionados

---
+++ Assets/Scripts/Selecionar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selecionar : MonoBehaviour
+{
+    public GameObject Icon;
+
+
+
+    void OnMouseOver()
+    {
+        Icon.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        Icon.SetActive(false);
+    }
+}
