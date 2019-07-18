e2c00caaced06583a61eb853f46e5fa717d2e124
GuiSR388 <guicrilopez@users.noreply.github.com>
2019-07-18 17:17:26 -0300

    Transição da camera para as regiões com um flick que não sei resolver ;-;

--- Assets/Scripts/Selecionar.cs
+++ Assets/Scripts/Selecionar.cs
@@ -5,16 +5,62 @@ using UnityEngine;
 public class Selecionar : MonoBehaviour
 {
     public GameObject Icon;
+    public GameObject Voltar;
+    public string Funcao;
 
-
+    public static bool Bylogah = false;
+    public static bool Ju = false;
+    public static bool Furry = false;
 
     void OnMouseOver()
     {
         Icon.SetActive(true);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Funcao == "ClickBylougah")
+            {
+                ClickBylougah();
+                print("Bylougah");
+            }
+            if (Funcao == "ClickJubatan")
+            {
+                ClickJubatan();
+                print("Jubathan");
+            }
+            if (Funcao == "ClickFurry")
+            {
+                ClickFurry();
+                print("Furry");
+            }
+        }
     }
 
     void OnMouseExit()
     {
         Icon.SetActive(false);
+    }
+
+    public void ClickJubatan()
+    {
+        Ju = true;
+        Voltar.SetActive(true);
+    }
+    public void ClickBylougah()
+    {
+        Bylogah = true;
+        Voltar.SetActive(true);
+    }
+    public void ClickFurry()
+    {
+        Furry = true;
+        Voltar.SetActive(true);
+    }
+
+    public void ClickVoltar()
+    {
+        Voltar.SetActive(false);
+        Bylogah = false;
+        Ju = false;
+        Furry = false;
     }
 }
---
+++ Assets/Scripts/TransitionCamera.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionCamera : MonoBehaviour
+{
+    public Transform[] views;
+    public float transitionSpeed;
+    Transform currentView;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if (Selecionar.Bylogah == false && Selecionar.Ju == false && Selecionar.Furry == false)
+        {
+            currentView = views[0];
+        }
+
+        if (Selecionar.Bylogah == true && Selecionar.Ju == false && Selecionar.Furry == false)
+        {
+            currentView = views[1];
+        }
+
+        if (Selecionar.Bylogah == false && Selecionar.Ju == true && Selecionar.Furry == false)
+        {
+            currentView = views[2];
+        }
+
+        if (Selecionar.Bylogah == false && Selecionar.Ju == false && Selecionar.Furry == true)
+        {
+            currentView = views[3];
+        }
+
+
+    }
+
+
+    void LateUpdate()
+    {
+
+        //Lerp position
+        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+
+        Vector3 currentAngle = new Vector3(
+         Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.transform.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+         Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.transform.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+         Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.transform.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
+
+        transform.eulerAngles = currentAngle;
+    }
+}
